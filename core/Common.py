79ad86b707848387def839281e187d766defe683
Don Fer <access2challenges@outlook.com>
2023-04-26 16:32:47 -0600

    Some refactoring

--- core/Common.py
+++ core/Common.py
@@ -1,7 +1,7 @@
 import time
 
 from selenium import webdriver
-from selenium.common import NoSuchElementException, TimeoutException
+from selenium.common import NoSuchElementException, TimeoutException, InvalidSelectorException
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
@@ -99,6 +99,12 @@ class Common:
                 self.HIGHLIGHT_DURATION,
                 self.HIGHLIGHT_COLOR, self.HIGHLIGHT_BORDER)
 
+    def get_element_by_text(self, locator_definition, target_text):
+        element_list = self.driver.find_elements(By.CSS_SELECTOR, locator_definition.replace("CSS:", ""))
+        for element in element_list:
+            if element.text == target_text:
+                return element
+
     def do_click_old(self, locator_type, locator_definition):
         self.get_element_old(locator_type, locator_definition).click()
 
@@ -109,9 +115,15 @@ class Common:
         for option in locator_definitions:
             try:
                 self.do_click(option)
+                print("<" + option + "> Selector found")
                 return True
             except NoSuchElementException:
-                print("<"+option+"> Selector not found trying next")
+                print("<" + option + "> Selector not found trying next")
+            except InvalidSelectorException:
+                print("<" + option + "> Selector is invalid please check it out.")
+
+    def do_click_by_text(self, ambiguous_locator_definition, target_text):
+        ActionChains(self.driver).move_to_element(self.get_element_by_text(ambiguous_locator_definition, target_text)).click().perform()
 
     def fill_input_text_old(self, locator_type, locator_definition, text):
         self.get_element_old(locator_type, locator_definition).send_keys(text)
--- test/Week-02/keep_it_easy_iii.py
+++ test/Week-02/keep_it_easy_iii.py
@@ -7,11 +7,17 @@ BROWSER = "chrome"
 # Test Elements
 CSS_SEARCH_INPUT_TEXT = "CSS:input[placeholder='Search the web without being tracked']"
 SEARCH_BUTTON = "CSS:.search__button.js-search-button"
-HONK_MENU_ICON = "CSS:span.ddgsi.ddgsi-horn"
+CHROME_SEARCH_BUTTON = "CSS:button[type='submit']"
+EDGE_HONK_MENU_ICON = "CSS:span.ddgsi.ddgsi-horn"
+CHROME_HONK_MENU_ICON = "CSS:.legacy-homepage_socialIcons__obCva.social-menu_button__2_gkB"
 HONK_MENU_ICON_BAD = "CSS:span.ddgsi.ddgsi-horm["
-HONK_MENU_TWITTER_ICON = "CSS:a[href='https://twitter.com/duckduckgo'] img"
+EDGE_HONK_MENU_TWITTER_ICON = "CSS:a[href='https://twitter.com/duckduckgo'] img"
+CHROME_HONK_MENU_TWITTER_ICON = "CSS:a[href='https://twitter.com/duckduckgo'] svg"
 EXPAND_DESCRIPTION_BUTTON = "CSS:#faq-btn-8"
+CHROME_EXPAND_DESCRIPTION_BUTTON = "CSS:.legacy-homepage_legacyButton__oUMB9.legacy-homepage_faqButton__maJxW span"
 LEARN_MORE_LINK = "CSS:#faq-answer-8 a"
+CHROME_LEARN_MORE_LINK = "CSS:a[href='https://spreadprivacy.com/how-does-the-duckduckgo-app-extension-protect-my" \
+                         "-privacy/'] "
 
 # Test Actions
 letsAutomate = Common(BROWSER, True)
@@ -19,14 +25,14 @@ letsAutomate = Common(BROWSER, True)
 letsAutomate.launch_site(TEST_URL, CSS_SEARCH_INPUT_TEXT)
 
 # letsAutomate.do_click(HONK_MENU_ICON)
-letsAutomate.do_click_from_options([HONK_MENU_ICON_BAD, HONK_MENU_ICON])
-letsAutomate.do_click(HONK_MENU_TWITTER_ICON)
+letsAutomate.do_click_from_options([EDGE_HONK_MENU_ICON, HONK_MENU_ICON_BAD, CHROME_HONK_MENU_ICON])
+letsAutomate.do_click_from_options([EDGE_HONK_MENU_TWITTER_ICON, CHROME_HONK_MENU_TWITTER_ICON])
 letsAutomate.page_back()
 
-letsAutomate.do_click(EXPAND_DESCRIPTION_BUTTON)
-letsAutomate.do_click(LEARN_MORE_LINK)
+letsAutomate.do_click_by_text(CHROME_EXPAND_DESCRIPTION_BUTTON, "How does the DuckDuckGo app & extension work?")
+letsAutomate.do_click_from_options([LEARN_MORE_LINK, CHROME_LEARN_MORE_LINK])
 letsAutomate.switch_browser_tab()
 
 letsAutomate.fill_input_text(CSS_SEARCH_INPUT_TEXT, "CSS Selector")
-letsAutomate.do_click(SEARCH_BUTTON)
+letsAutomate.do_click_from_options([SEARCH_BUTTON, CHROME_SEARCH_BUTTON])
 letsAutomate.page_back()
