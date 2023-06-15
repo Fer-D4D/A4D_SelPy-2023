e9af4afc4cfbac15f9037e5b298fe8e69c2774cb
LauraJCurtidorH <lau.curti2@hotmail.com>
2023-06-15 17:18:06 -0500

    new page

--- Team/lau/test/Week1/sauce_test_methods.py
+++ Team/lau/test/Week1/sauce_test_methods.py
@@ -36,6 +36,19 @@ class Page:
     def do_click(self, by_locator, locator):
         return self.Driver.find_element(by_locator, locator).click()
 
+    def get_text_to_element(self, by_locator, locator):
+        return self.Driver.find_element(by_locator, locator).text
+    def refresh_to_page(self):
+        return self.refresh_to_page()
+
+    def back_to_page(self):
+        return self.back_to_page()
+    def choose_dropdown_option(self, *args):
+        self.do_click(args[0], args[1])
+        self.fill_text_to_element(args[2], args[3], args[4])
+        self.do_click(args[5], args[6])
+
+
     # def do_login(self, *args):
     #     print(args)
     #     self.fill_text_to_element(args[0], args[1], args[2])
