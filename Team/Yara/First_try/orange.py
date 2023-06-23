5f17210ff79e47d9a8a964cf2a5e1d759925cac1
Yara_Mendoza <mel.avila0906@gmail.com>
2023-06-23 17:28:47 -0600

    Yara commit add a method for login hasta aui funciona bien

--- Team/Yara/First_try/orange.py
+++ Team/Yara/First_try/orange.py
@@ -1,8 +1,12 @@
+import time
+
 from selenium import webdriver
 from selenium.common import ElementClickInterceptedException
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+
+
 
 
 class TinyCore:
@@ -19,12 +23,20 @@ class TinyCore:
     def login_success(self,locator,user,locator_pass,password,locator_button):
         try:
             self.DRIVER.find_element(By.NAME,locator).send_keys(user)
-            self.DRIVER.find_element(By.NAME, locator_pass).send_keys(password)
-            self.DRIVER.find_element(By.XPATH, locator_button).click()
+            self.DRIVER.find_element(By.NAME,locator_pass).send_keys(password)
+            self.DRIVER.find_element(By.CSS_SELECTOR,locator_button).click()
 
         except ElementClickInterceptedException:
 
             print("Re-clicking")
+
+    def oneSec(func):
+        def wrapper(*args, **kwargs):
+            time.sleep(1)
+            result = func(*args, **kwargs)
+            return result
+
+        return wrapper
 
 
 
--- Team/Yara/First_try/orange_frame_test.py
+++ Team/Yara/First_try/orange_frame_test.py
@@ -8,11 +8,12 @@ ADMIN_USER = "Admin"
 ADMIN_PASS = "admin123"
 locator_user = "username"
 locator_pass = "password"
-locator_login = "$x('//button[@label=Submit]')"
+locator_login = "#app > div.orangehrm-login-layout > div > div.orangehrm-login-container > div > div.orangehrm-login-slot > div.orangehrm-login-form > form > div.oxd-form-actions.orangehrm-login-action > button"
 
 
 
 print(lets_automate.browsers())
+
 
 
 lets_automate.set_page()
@@ -22,6 +23,5 @@ time.sleep(5)
 class_login = "Username"
 
 lets_automate.login_success(locator_user,ADMIN_USER,locator_pass,ADMIN_PASS,locator_login)
-
+time.sleep(10)
 print("test")
-time.sleep(5)
