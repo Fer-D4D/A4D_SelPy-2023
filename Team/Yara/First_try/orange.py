f22b2c4d0d561edf39b4c65b1a37a1cc79d311cc
Yara_Mendoza <mel.avila0906@gmail.com>
2023-06-29 11:05:37 -0600

    Yara commit a new methods implemented to good practices

--- Team/Yara/First_try/orange.py
+++ Team/Yara/First_try/orange.py
@@ -6,48 +6,43 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
+PAGE = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+DRIVER = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+DRIVER.implicitly_wait(5)
+#page
+DRIVER.get(PAGE)
+
+#In this class can fin the locators
+class locators:
+
+    locator_user = "username"
+    locator_pass = "password"
+    locator_login = "#app > div.orangehrm-login-layout > div > div.orangehrm-login-container > div > div.orangehrm-login-slot > div.orangehrm-login-form > form > div.oxd-form-actions.orangehrm-login-action > button"
+
+#In this class can find the data to test
+class data:
+    ADMIN_USER = "Admin"
+    ADMIN_PASS = "admin123"
 
 
+def get_user(locator_user, user):
+    DRIVER.find_element(By.NAME, locator_user).send_keys(user)
 
-class TinyCore:
-    Driver = None
+def get_pass(locator_pass, password):
+    DRIVER.find_element(By.NAME, locator_pass).send_keys(password)
 
-    def browsers(self):
-        self.DRIVER = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-        return self.DRIVER
-
-    def set_page(self):
-        self.DRIVER.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-        return self.DRIVER
-
-    def login_success(self,locator,user,locator_pass,password,locator_button):
-        try:
-            self.DRIVER.find_element(By.NAME,locator).send_keys(user)
-            self.DRIVER.find_element(By.NAME,locator_pass).send_keys(password)
-            self.DRIVER.find_element(By.CSS_SELECTOR,locator_button).click()
-
-        except ElementClickInterceptedException:
-
-            print("Re-clicking")
-
-    def oneSec(func):
-        def wrapper(*args, **kwargs):
-            time.sleep(1)
-            result = func(*args, **kwargs)
-            return result
-
-        return wrapper
+def login_success(locator_button):
+    try:
+        DRIVER.find_element(By.CSS_SELECTOR, locator_button).click()
+    except ElementClickInterceptedException:
+        print("Re-clicking")
 
 
+get_user(locators.locator_user, data.ADMIN_USER)
 
-
-
-
-
-
-
-
-
+get_pass(locators.locator_pass, data.ADMIN_PASS)
+login_success(locators.locator_login)
 
 
 
--- Team/Yara/First_try/orange_frame_test.py
+++ Team/Yara/First_try/orange_frame_test.py
@@ -1,9 +1,9 @@
 import time
 from asyncio import wait
 
-from Team.Yara.First_try.orange import TinyCore
+from Team.Yara.First_try.orange import locators
 
-lets_automate = TinyCore()
+
 ADMIN_USER = "Admin"
 ADMIN_PASS = "admin123"
 locator_user = "username"
@@ -12,16 +12,3 @@ locator_login = "#app > div.orangehrm-login-layout > div > div.orangehrm-login-c
 
 
 
-print(lets_automate.browsers())
-
-
-
-lets_automate.set_page()
-
-time.sleep(5)
-
-class_login = "Username"
-
-lets_automate.login_success(locator_user,ADMIN_USER,locator_pass,ADMIN_PASS,locator_login)
-time.sleep(10)
-print("test")
