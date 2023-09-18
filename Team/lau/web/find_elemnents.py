0b544cd92d5c7ebf99db2ef082f81c4a5b133ddd
LauraJCurtidorH <lau.curti2@hotmail.com>
2023-09-18 16:44:09 -0500

    ADDING drivers

--- Team/lau/web/find_elemnents.py
+++ Team/lau/web/find_elemnents.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
+driver = webdriver.Firefox()
 try:
     driver.get('https://the-internet.herokuapp.com/')
     driver.find_element(By.XPATH,'//a[contains(@href,"login")]')
+
     els =driver.find_element(By.TAG_NAME, 'a')
     print(f'There were {len(els)} a elements')
     els = driver.find_element(By.TAG_NAME, 'foo')
