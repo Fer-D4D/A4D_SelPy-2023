95f0313e1ae1e3c16fea3d388dc9a8fe205f9280
LauraJCurtidorH <lau.curti2@hotmail.com>
2023-10-27 12:53:35 -0500

    ADDING iteration

--- Team/lau/web/basic_iteraction.py
+++ Team/lau/web/basic_iteraction.py
@@ -33,3 +33,27 @@ try:
 
 finally:
     driver.quit()
+
+    #OTHER ITERATION
+    # other iteration with the browser driver.back(), driver.forward(),driver.refresh()
+    ## get the html source as a string source= driver.page_source
+    ### execute javascript in the context od the page and get the result
+    ### res = driver.execute_script('return 2+2;')
+    #### screenshot driver.save_screenshot('evidence.png')
+
+    #WINDOWS,FRAMES AND ALERT
+    #windows has his own id, it shows the list of strings for all the windows
+    # name_list = driver.windows_handles
+    ## switch to a given window by its handle driver.switch_to.window(handle)
+    ### get the handle of the currently active window handle= driver.current_window_handle
+    #### Maximize and minimize driver.maximize_window() or driver.minimize_window()
+    ##### get windows size driver.get_window_rect()
+    ###### set windows size driver.set_window_rect(x=50, y=50, height=100, width=200)
+    #FRAME AND IFRAMES : PAGE IN THE MIDLE OF WEB PAGE WE USUALLY USE IFRAMES
+    #driver.switch_to.frame(frame)  name of frame, number,
+    ## driver.switch_to.parent_frame()  or driver.switch_to.default_content()
+    #ALERTS
+    #alert = driver.switch_to.alert
+    ## acept alert alert.accept() to dissmiss the not accept option alert.dismiss()
+    ### text displayed in the alert text=alert.text
+    #### send keysstrokes alert.send_keys('name key')
