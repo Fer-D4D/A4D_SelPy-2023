3a790a77257856dadd8922fdbf0a6e7bda40e00d
LauraJCurtidorH <lau.curti2@hotmail.com>
2023-10-30 14:33:09 -0500

    ADDING cp

--- Team/lau/test/mobile_appium/sessions_ios.py
+++ Team/lau/test/mobile_appium/sessions_ios.py
@@ -1,7 +1,24 @@
 from appium import webdriver
 from os import path
 
-CUR_DIR = path.dirname(path.abspath(__file__))
-APP = path.join(CUR_DIR, 'TheApp.app.zip')
-APPIUM = 'http://'
-#location
+CUR_DIR = path.dirname(path.abspath(__file__)) #variable to the field incounter
+APP = path.join(CUR_DIR, 'TheApp.app.zip') #get actual path
+APPIUM = 'http://localhost:4723' #location url to research the service
+#define capabilities
+
+CAPS = {
+    'platformName':'IOS',
+    'platformVersion':'13.6',
+    'deviceName':'iPhone 14 PRO',
+    'automationName': 'XCUITest',
+    'app': APP,
+}
+
+driver = webdriver.Remote(
+    command_executor=APPIUM,
+    desired_capabilities=CAPS
+)
+
+driver.quit()
+
+
