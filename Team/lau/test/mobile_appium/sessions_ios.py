e7e9a95395931ae0f260eeb17ddfc837d2b91fcf
LauraJCurtidorH <lau.curti2@hotmail.com>
2023-11-24 10:04:33 -0500

    ADDING android

--- Team/lau/test/mobile_appium/sessions_ios.py
+++ Team/lau/test/mobile_appium/sessions_ios.py
@@ -1,21 +1,31 @@
 from appium import webdriver
 from os import path
+from appium.options.ios import XCUITestOptions
+
 CUR_DIR = path.dirname(path.abspath(__file__)) # variable to the field encounter
 APP = path.join(CUR_DIR, 'TheApp.app.zip') # get actual path
-APPIUM = 'http://127.0.0.1:4723/' # location URL to reach the service
+APPIUM = 'http://localhost:4723/' # location URL to reach the service
+
 # define capabilities
 CAPS = {
     'platformName': 'iOS',
     'platformVersion': '16.2',
     'deviceName': 'iPhone 14 Pro',
     'automationName': 'XCUITest',
-    'app': APP,
+    'app': APP
 }
+
+capabilities_option = XCUITestOptions().load_capabilities(CAPS)
 
 driver = webdriver.Remote(
     command_executor=APPIUM,
-    options=None
+    options=capabilities_option
 )
-driver.quit()
+
+
+#driver = webdriver.Remote(
+    #command_executor=APPIUM,
+    #options=None)
+#driver.quit()
 
 
